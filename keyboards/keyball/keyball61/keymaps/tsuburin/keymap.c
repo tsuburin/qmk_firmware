a68660a0a78d751a8c75c7afd4cc85fe47a5f621
tsuburin <tsuburin@gmail.com>
2024-02-09 11:15:17 +0900

    キーマップ変更

--- keyboards/keyball/keyball61/keymaps/tsuburin/keymap.c
+++ keyboards/keyball/keyball61/keymaps/tsuburin/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                   KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_LBRC  ,
     KC_CAPS  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                   KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , TO(0)    ,             MO(1)    , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , TO(1)    ,
-    TO(0)    , MO(2)    , KC_LGUI  , KC_LALT  ,LT(1,KC_LNG2),KC_SPC , KC_DEL   ,             KC_BSPC  , KC_ENT   ,                                  TO(3)    , TO(2)
+    TO(0)    , MO(2)    , KC_LGUI  , KC_LALT  , TO(0)    , KC_SPC   , KC_DEL   ,             KC_BSPC  , KC_ENT   ,                                  TO(3)    , TO(2)
   ),
 
   [1] = LAYOUT_right_ball(
